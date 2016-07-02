2bc95a25046cb3939a62df91af48896e1ee783c5
Daniel Supernault <danielsupernault@gmail.com>
2016-07-01 19:19:56 -0600

    Add new methods
    The following methods were added:
    - fsck
    - gc
    - stat
    - version
    - resolve
    - bw
    - swarmAddrs
    - swarmLocalAddrs

--- src/Contracts/Driver.php
+++ src/Contracts/Driver.php
@@ -18,7 +18,7 @@ trait Driver {
         'file'    => $content
         ];
         $cmd = null;
-        $response = parent::handleCall($api, $cmd);
+        $response = (new self())->handleCall($api, $cmd);
         return json_decode($response);
     }
 
@@ -35,7 +35,7 @@ trait Driver {
         'file'    => null
         ];
         $cmd = null;
-        $response = parent::handleCall($api, $cmd);
+        $response = (new self())->handleCall($api, $cmd);
         return json_decode($response);
     }
 
@@ -52,7 +52,7 @@ trait Driver {
         'file'    => null
         ];
         $cmd = null;
-        $response = parent::handleCall($api, $cmd);
+        $response = (new self())->handleCall($api, $cmd);
         return json_decode($response);
     }
 
@@ -69,7 +69,7 @@ trait Driver {
         'file'    => null
         ];
         $cmd = null;
-        $response = parent::handleCall($api, $cmd);
+        $response = (new self())->handleCall($api, $cmd);
         return $response;
     }
 
@@ -86,7 +86,7 @@ trait Driver {
         'file'    => null
         ];
         $cmd = null;
-        $response = parent::handleCall($api, $cmd);
+        $response = (new self())->handleCall($api, $cmd);
         return json_decode($response);
     }
 
@@ -103,7 +103,7 @@ trait Driver {
         'file'    => null
         ];
         $cmd = null;
-        $response = parent::handleCall($api, $cmd);
+        $response = (new self())->handleCall($api, $cmd);
         return json_decode($response);
     }
 
@@ -120,7 +120,7 @@ trait Driver {
         'file'    => null
         ];
         $cmd = null;
-        $response = parent::handleCall($api, $cmd);
+        $response = (new self())->handleCall($api, $cmd);
         return json_decode($response);
     }
 
@@ -137,7 +137,153 @@ trait Driver {
         'file'    => null
         ];
         $cmd = null;
-        $response = parent::handleCall($api, $cmd);
+        $response = (new self())->handleCall($api, $cmd);
         return json_decode($response);
     }
+
+    /**
+     * Removes repo lockfiles.
+     * @return object Guzzle Response.
+     */
+    public static function fsck()
+    {
+        $api = [
+        'path'    => 'http://localhost:5001/api/v0/repo/fsck',
+        'method'  => 'GET',
+        'query'   => null,
+        'file'    => null
+        ];
+        $cmd = null;
+        $response = (new self())->handleCall($api, $cmd);
+        return json_decode($response);
+    }
+
+    /**
+     * Sweep the local set of stored objects and remove ones that are not
+     * pinned in order to reclaim hard disk space.
+     * @return object Guzzle Response.
+     */
+    public static function gc()
+    {
+        $api = [
+        'path'    => 'http://localhost:5001/api/v0/repo/gc',
+        'method'  => 'GET',
+        'query'   => null,
+        'file'    => null
+        ];
+        $cmd = null;
+        $response = (new self())->handleCall($api, $cmd);
+        return json_decode($response);
+    }
+
+    /**
+     * Get stats for the currently used repo. This is a plumbing command that 
+     * will scan the local set of stored objects and print repo statistics.
+     * @return object Guzzle Response.
+     */
+    public static function stat()
+    {
+        $api = [
+        'path'    => 'http://localhost:5001/api/v0/repo/stat',
+        'method'  => 'GET',
+        'query'   => null,
+        'file'    => null
+        ];
+        $cmd = null;
+        $response = (new self())->handleCall($api, $cmd);
+        return json_decode($response);
+    }
+
+    /**
+     * Show repo version.
+     * @return object Guzzle Response.
+     */
+    public static function version()
+    {
+        $api = [
+        'path'    => 'http://localhost:5001/api/v0/repo/version',
+        'method'  => 'GET',
+        'query'   => null,
+        'file'    => null
+        ];
+        $cmd = null;
+        $response = (new self())->handleCall($api, $cmd);
+        return json_decode($response);
+    }
+
+    /**
+     * There are a number of mutable name protocols that can link among 
+     * themselves and into IPNS. For example IPNS references can (currently) 
+     * point at an IPFS object, and DNS links can point at other DNS links, 
+     * IPNS entries, or IPFS objects. This command accepts any of these 
+     * identifiers and resolves them to the referenced item.
+     * @return object Guzzle Response.
+     */
+    public static function resolve($arg)
+    {
+        $api = [
+        'path'    => 'http://localhost:5001/api/v0/resolve',
+        'method'  => 'GET',
+        'query'   => ['arg'=>$query],
+        'file'    => null
+        ];
+        $cmd = null;
+        $response = (new self())->handleCall($api, $cmd);
+        return json_decode($response);
+    }
+
+    /**
+     * Print ipfs bandwidth information.
+     * @return object Guzzle Response.
+     */
+    public static function bw($peer=null)
+    {
+        $query = null;
+        if($peer !== null) {
+          $query = ['peer' => $peer];
+        }
+        $api = [
+        'path'    => 'http://localhost:5001/api/v0/stats/bw',
+        'method'  => 'POST',
+        'query'   => $query,
+        'file'    => null
+        ];
+        $cmd = null;
+        $response = (new self())->handleCall($api, $cmd);
+        return json_decode($response);
+    }
+
+    /**
+     * List known addresses.
+     * @return object Guzzle Response.
+     */
+    public static function swarmAddrs()
+    {
+        $api = [
+        'path'    => 'http://localhost:5001/api/v0/swarm/addrs',
+        'method'  => 'GET',
+        'query'   => null,
+        'file'    => null
+        ];
+        $cmd = null;
+        $response = (new self())->handleCall($api, $cmd);
+        return json_decode($response);
+    } 
+
+    /**
+     * List all local addresses the node is listening on.
+     * @return object Guzzle Response.
+     */
+    public static function swarmLocalAddrs()
+    {
+        $api = [
+        'path'    => 'http://localhost:5001/api/v0/swarm/addrs/local',
+        'method'  => 'GET',
+        'query'   => null,
+        'file'    => null
+        ];
+        $cmd = null;
+        $response = (new self())->handleCall($api, $cmd);
+        return json_decode($response);
+    } 
 }
